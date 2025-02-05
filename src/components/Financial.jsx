b9c4ae24b31e4ec36ad8bb51c51a8274a1616104
Jack Stange <jstange@contfire.local>
2025-02-05 14:50:51 -0500

    retry on video stream i frame

--- src/components/Financial.jsx
+++ src/components/Financial.jsx
@@ -17,57 +17,8 @@ export function Financial() {
         src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/FireIsFast.mp4">
     </iframe>
 
-    <iframe
-        width="600"
-        height="450"
-        style="border:0"
-        loading="lazy"
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Home_Fire_Evacuation_3rd_Grade.mp4">
-    </iframe>
-
-    <iframe
-        width="600"
-        height="450"
-        style="border:0"
-        loading="lazy"
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Middlesex_County_Fire_Prevention.mp4">
-    </iframe>
-
-    <iframe
-        width="600"
-        height="450"
-        style="border:0"
-        loading="lazy"
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Sparkys_ABCs_of_Fire_Safety.mp4">
-    </iframe>
-
-    <iframe
-        width="600"
-        height="450"
-        style="border:0"
-        loading="lazy"
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/StopDropRoll.mp4">
-    </iframe>
-
-    <iframe
-        width="600"
-        height="450"
-        style="border:0"
-        loading="lazy"
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Home_Fire_Evacuation_3rd_Grade.mp4">
-    </iframe>
-    </div>
-    </center>
+</div>
+</center>
     
     )
 }
