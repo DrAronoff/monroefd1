520fc99f01f53cd15decd5169d38ab93d0f3e02c
Jack Stange <jstange@contfire.local>
2025-02-05 14:27:56 -0500

    removed videos

--- src/components/Financial.jsx
+++ src/components/Financial.jsx
@@ -4,18 +4,69 @@ export function Financial() {
     return (
         center>
         <div class="right2">
-            <br/>
-            <br/>
+        <br/>
+        <br/>
 
-        <iframe
-            width="600"
-            height="450"
-            style="border:0"
-            loading="lazy"
-            allowfullscreen
-            referrerpolicy="no-referrer-when-downgrade"
-            src="FirePreventionVideos/FireIsFast.mp4">
-        </iframe>
-        </div>
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/FireIsFast.mp4">
+    </iframe>
+
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Home_Fire_Evacuation_3rd_Grade.mp4">
+    </iframe>
+
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Middlesex_County_Fire_Prevention.mp4">
+    </iframe>
+
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Sparkys_ABCs_of_Fire_Safety.mp4">
+    </iframe>
+
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/StopDropRoll.mp4">
+    </iframe>
+
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://diuy0t6boke2n.cloudfront.net/FirePreventionVideos/Home_Fire_Evacuation_3rd_Grade.mp4">
+    </iframe>
+    </div>
+    
     )
 }
