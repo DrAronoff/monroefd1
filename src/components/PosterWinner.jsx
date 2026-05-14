e45eea4d68cca2ac792e9a0843db705fb75df1b8
Jack Stange <jstange@contfire.local>
2026-05-14 10:00:16 -0400

    meeting time change

--- src/components/PosterWinner.jsx
+++ src/components/PosterWinner.jsx
@@ -11,7 +11,6 @@ export function PosterWinner() {
             <br/>
         <center>
         <div class="right2">
-<h1>2024-2025 Fire Prevention Poster Contest Winners</h1>
 <img src="https://diuy0t6boke2n.cloudfront.net/poster+contest/IMG_8862.jpg" alt="Poster"></img>
 <img src="https://diuy0t6boke2n.cloudfront.net/poster+contest/IMG_8859.jpg" alt="Poster"></img>
 <img src="https://diuy0t6boke2n.cloudfront.net/poster+contest/IMG_8864.jpg" alt="Poster"></img>
